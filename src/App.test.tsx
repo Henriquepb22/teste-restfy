fabe7c289f5dc55d81d4bf42a11cbb551fa7e609
Henriquepb22 <henriquepbdev@gmail.com>
2020-08-11 14:17:58 -0300

    removed tests and moved dependencies to devdependencies

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders app title', () => {
-  const { getByText } = render(<App />);
-  const title = getByText(/hello restfy/i);
-  expect(title).toBeInTheDocument();
-});
